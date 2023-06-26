190a60effa18d3c2e199b971389a33e4cd89896e
lrodriguez <lrodriguez@trafficlight.mx>
2023-06-26 12:55:51 -0600

    segunda version completa

--- A_BotoneraSimple.ino
+++ A_BotoneraSimple.ino
@@ -7,7 +7,13 @@ const int buttonPin4 = 5;         // input pin for pushbutton
 const int buttonPin5 = 6;         // input pin for pushbutton
 const int buttonPin6 = 7;         // input pin for pushbutton
 
-boolean flag = true; 
+boolean flag1 = true; 
+boolean flag2 = true; 
+boolean flag3 = true; 
+boolean flag4 = true; 
+boolean flag5 = true; 
+boolean flag6 = true; 
+
 
 void setup() {
   // make the pushButton pin an input:
@@ -30,16 +36,65 @@ void loop() {
   int buttonState4 = digitalRead(buttonPin4);
   int buttonState5 = digitalRead(buttonPin5);
   int buttonState6 = digitalRead(buttonPin6);
+
+  switch (buttonState1){
+
+  }
   /////////* Boton f */////////
   // if the button state has changed,
   if (buttonState1 == HIGH){
-    flag = false;
+    flag1 = false;
   }
-  else if((buttonState1 == LOW) && (flag == false)){
-    flag = true;
+  else if((buttonState1 == LOW) && (flag1 == false)){
+    flag1 = true;
     Keyboard.print("f");
+  }
+  /////////* Boton g */////////
+  // if the button state has changed,
+  if (buttonState2 == HIGH){
+    flag2 = false;
+  }
+  else if((buttonState2 == LOW) && (flag2 == false)){
+    flag2 = true;
+    Keyboard.print("g");
+  }
+  /////////* Boton h */////////
+  // if the button state has changed,
+  if (buttonState3 == HIGH){
+    flag3 = false;
+  }
+  else if((buttonState3 == LOW) && (flag3 == false)){
+    flag3 = true;
+    Keyboard.print("h");
+  }
+  /////////* Boton j */////////
+  // if the button state has changed,
+  if (buttonState4 == HIGH){
+    flag4 = false;
+  }
+  else if((buttonState4 == LOW) && (flag4 == false)){
+    flag4 = true;
+    Keyboard.print("j");
+  }
+  /////////* Boton k */////////
+  // if the button state has changed,
+  if (buttonState5 == HIGH){
+    flag5 = false;
+  }
+  else if((buttonState5 == LOW) && (flag5 == false)){
+    flag5 = true;
+    Keyboard.print("k");
+  }
+  /////////* Boton l */////////
+  // if the button state has changed,
+  if (buttonState6 == HIGH){
+    flag6 = false;
+  }
+  else if((buttonState6 == LOW) && (flag6 == false)){
+    flag6 = true;
+    Keyboard.print("l");
   }
   
   //Delay time
-  delay(100);
+  delay(150);
 }
